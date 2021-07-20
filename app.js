b1fa0253b6354c1afe005dd8f48eb1dc7188647f
Kevin <kevin.morales05@epn.edu.ec>
2021-07-20 17:27:03 -0500

    Subo informacion de la API REST de Paypal

---
+++ app.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const engines = require("consolidate");
+const paypal = require("paypal-rest-sdk");
+
+const app = express();
+
+app.engine("ejs", engines.ejs);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+
+//configuracion de Paypal, colocar el id del cliente y su secreto
+
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id:
+        "AUaLcL0v9fhe7eqZNTuWD_GOPIYe1wgfZEWfH8NJRUZaw8MlosCQPhU0ZsIfF2N-VgohcLNRgt2XdY5F",
+    client_secret:
+        "EGxtbS-6um8OWnOgDquuF7dltserVNio9-vuvaRvLdy6Idh-1NDDSoQf1WN4Y5bkaLGvNeOhL_Gt8RPQ"
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+//creacion de la ruta /paypal
+app.get("/paypal", (req, res) => {
+    var create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel"
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "item",
+                            sku: "item",
+                            price: "1.00",
+                            currency: "USD",
+                            quantity: 1
+                        }
+                    ]
+                },
+                amount: {
+                    currency: "USD",
+                    total: "1.00"
+                },
+                description: "This is the payment description."
+            }
+        ]
+    };
+
+    paypal.payment.create(create_payment_json, function(error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("Create Payment Response");
+            console.log(payment);
+            res.redirect(payment.links[1].href); //redirijo a los links de pago
+            //res.send('Ok!')
+          
+        }
+    });
+});
+
+//manejar el exito
+app.get("/success", (req, res) => {
+    //informacion del pago
+    var PayerID = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+    var execute_payment_json = {
+        payer_id: PayerID,
+        transactions: [
+            {
+                amount: {
+                    currency: "USD",
+                    total: "1.00"
+                }
+            }
+        ]
+    };
+//ejecusion del pago
+    paypal.payment.execute(paymentId, execute_payment_json, function(
+        error,
+        payment
+    ) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log("Get Payment Response");
+            console.log(JSON.stringify(payment));
+            res.render("success");
+        }
+    });
+});
+//cancelacion del pago
+app.get("cancel", (req, res) => {
+    res.render("cancel");
+});
+
+
+//servidor
+app.listen({port: process.env.PORT || 3000}, () => {
+    console.log("Server is running");
+});
